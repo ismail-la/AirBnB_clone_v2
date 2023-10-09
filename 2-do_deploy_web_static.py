16bb88354f1266b02e969e74b5e2a75a1450e4ea
ismail-la <lahbariismail@gmail.com>
2023-10-08 18:33:08 -0700

    0x03. AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers,
- using the function do_deploy
+"""Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers,using the function do_deploy
 """
 from fabric.api import *
 from fabric.operations import put
