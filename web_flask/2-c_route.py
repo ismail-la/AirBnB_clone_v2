dd3a02086c35d4b71d36fa36f24544b65bc5beae
ismail-la <lahbariismail@gmail.com>
2023-10-21 16:08:29 -0700

    0x04. AirBnB clone - Web framework

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,48 +19,35 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# -This Decorator tells Flask what URL should trigger our function.
+# -strict_slashes=False: option means the route is defined without trailing
+# slashes and Flask redirects all URLs with trailing slashes to this route.
 @app.route('/', strict_slashes=False)
-""" -This Decorator tells Flask what URL should trigger our function.
--strict_slashes=False: option means the route is defined without trailing
-slashes and Flask redirects all URLs with trailing slashes to this route.
-"""
-
-
 def show_hello_HBNB():
     """This function is triggered when the user visits the root URL"""
     return 'Hello HBNB!'
-    """Returns the string ‘Hello HBNB!"""
 
 
+# -This decorator tells Flask to trigger the show_HBNB() function when
+#  the ‘/hbnb’ URL is visited.
 @app.route('/hbnb', strict_slashes=False)
-""" This decorator tells Flask to trigger the show_HBNB() function when
-the ‘/hbnb’ URL is visited.
-"""
-
-
 def show_HBNB():
     """This function is triggered when the user visits the ‘/hbnb’ URL"""
     return 'HBNB'
-    """returns the string ‘HBNB"""
 
 
+# -This decorator tells Flask to trigger the show_C(text) function when the
+#  ‘/c/<text>’ URL is visited. <text> is a variable part of the URL.
+# -Returns a string that starts with 'C ’ and ends with the <text> from
+#   the URL, with underscores replaced by spaces.
 @app.route('/c/<text>', strict_slashes=False)
-"""
-This decorator tells Flask to trigger the show_C(text) function when the
-‘/c/<text>’ URL is visited. <text> is a variable part of the URL.
-"""
-
-
 def show_C(text):
     """This function is triggered when the user visits the ‘/c/<text>’ URL"""
     return 'C {}'.format(text.replace('_', ' '))
-    """eturns a string that starts with 'C ’ and ends with the <text> from
-    the URL, with underscores replaced by spaces.
-    """
 
 
 # -If this script is run directly (not imported), then it will run the app
 #   on host 0.0.0.0 (which makes it publicly available) and on port 5000.
-# -Set socket to run the app on.
+# Set socket to run the app on.
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
