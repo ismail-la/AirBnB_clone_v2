b141cdb0b8a712633fda5e8e3a12885d43efaf7e
ismail-la <lahbariismail@gmail.com>
2023-10-22 17:09:23 -0700

    0x04. AirBnB clone - Web framework

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application:
--web application  listens on 0.0.0.0, port 5000.
--Routes:
-   /hbnb_filters: : display a HTML page like 6-index.html
-   ....see rest in readme file
+"""Script that starts a Flask web application
+ - web application must be listening on 0.0.0.0, port 5000
+ ...see the rest in readme file
 """
 from models import storage
 from flask import render_template
@@ -15,7 +13,9 @@ app = Flask(__name__)
 
 @app.route("/hbnb_filters", strict_slashes=False)
 def hbnb_filters():
-    """Displays the main HBnB filters HTML page"""
+    """
+	Displays the main HBnB filters HTML page
+	"""
     states = storage.all("State")
     amenities = storage.all("Amenity")
     return render_template("10-hbnb_filters.html",
@@ -24,7 +24,9 @@ def hbnb_filters():
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session"""
+    """
+	Remove the current SQLAlchemy session
+	"""
     storage.close()
 
 
